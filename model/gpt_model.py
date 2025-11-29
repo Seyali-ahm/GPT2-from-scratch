b5eb162ef557a9d7e02919607f0c2497db2fa39f
Seyali-ahm <seyyedaliahmadi9@gmail.com>
2025-11-30 00:34:24 +0100

    Implementing generation inside GPTmodel

--- model/gpt_model.py
+++ model/gpt_model.py
@@ -6,6 +6,7 @@ from layers.layernorm import LayerNorm
 class GPTModel(nn.Module):
     def __init__(self, cfg):
         super().__init__()
+        self.cfg = cfg
         self.tok_emb = nn.Embedding(cfg["vocab_size"], cfg["emb_dim"])
         self.pos_emb = nn.Embedding(cfg["context_length"], cfg["emb_dim"])
         self.drop_emb = nn.Dropout(cfg["drop_rate"])
@@ -29,3 +30,32 @@ class GPTModel(nn.Module):
         x = self.final_norm(x)
         logits = self.out_head(x)
         return logits
+    
+    @torch.no_grad()
+    def generate(self, idx, max_new_tokens, temperature=0.0, top_k=None, eos_id=None):
+        cfg = self.cfg
+        context_size = cfg["context_length"]
+
+        for _ in range(max_new_tokens):
+            idx_cond = idx[:, -context_size:]
+            logits = self(idx_cond)
+            logits = logits[:, -1, :]
+
+            if top_k is not None:
+                top_logits, _ = torch.topk(logits, top_k)
+                min_val = top_logits[:, -1]
+                logits = torch.where(logits < min_val, float("-inf"), logits)
+
+            if temperature > 0.0:
+                logits = logits / temperature
+                probs = torch.softmax(logits, dim=-1)
+                idx_next = torch.multinomial(probs, num_samples=1)
+            else:
+                idx_next = torch.argmax(logits, dim=-1, keepdim=True)
+
+            if eos_id is not None and idx_next.item() == eos_id:
+                break
+
+            idx = torch.cat((idx, idx_next), dim=1)
+        
+        return idx
