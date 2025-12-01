e253f03aebbe023c895674f15ec48827e93f438e
Seyali-ahm <seyyedaliahmadi9@gmail.com>
2025-12-01 21:57:24 +0100

    Adding 2 modes for training and generating for interactive env

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-import tiktoken
-from config.config import CONFIG
+from train.train import train_main
+from utils.gpt2.pretrained_chat import run_pretrained_chat
 
 
-tokenizer = tiktoken.get_encoding("gpt2")
+def main():
+    print("Choose an option:")
+    print("1) Train GPT-2 from scratch")
+    print("2) Pretrain GPT-2")
 
+    choice = input("Enter 1 or 2: ")
 
+    if choice == "1":
+        print("[INFO] Starting training...")
+        train_main()   # your train function
+
+    elif choice == "2":
+        print("[INFO] Loading pretrained weights...")
+        run_pretrained_chat()
+if __name__ == "__main__":
+    main()
